992e1e2488fc998969ddf17639ee8408cafbbee1
vbboyd <vbboyd123456@yahoo.com>
2016-02-19 16:05:29 -0700

    Refactor middleware

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@
 
 var middleware = {
 	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
+		console.log('private route hit!!');
 		next();
 	},
 	logger: function (req, res, next) {
